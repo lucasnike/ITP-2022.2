9779f7e79e577aa505d2dcbd7c5d863ea2bab826
Lucas Vinícius <lucas4cristal@gmail.com>
2022-09-23 13:31:48 -0300

    Prova finalizada

--- Unidade 1/Week 5/LoP/Prova/diferenca_conjuntos.c
+++ Unidade 1/Week 5/LoP/Prova/diferenca_conjuntos.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 
 void bubble(int size, int *numbers);
+void append(int size, int *arr, int n);
 
 int main(){
    int set_size_1 = 0, set_size_2 = 0, qtdUnicos = 0;
@@ -22,9 +23,11 @@ int main(){
       scanf("%d", &set2[i]);
    }
 
+   // Organiza os dois conjuntos
    bubble(set_size_1, set1);
    bubble(set_size_2, set2);
    
+   // Percorre os primeiro array e verifica se tem 1 elemento igual no segundo array
    for (int i = 0; i < set_size_1; i++)
    {
       int qtd_coincidentes = 0;
@@ -43,6 +46,7 @@ int main(){
       }
    }
 
+   // Percorre o segundo array e verifica se tem 1 elemento igual no primeiro array
    for (int i = 0; i < set_size_2; i++)
    {
       int qtd_coincidentes = 0;
@@ -62,6 +66,56 @@ int main(){
    }
 
    int set3[qtdUnicos];
+
+   for (int i = 0; i < qtdUnicos; i++)
+   {
+      set3[i] = 0;
+   }
+   
+   // Percorre os primeiro array e verifica se tem 1 elemento igual no segundo array
+   for (int i = 0; i < set_size_1; i++)
+   {
+      int qtd_coincidentes = 0;
+      for (int j = 0; j < set_size_2; j++)
+      {
+         if (set1[i] == set2[j])
+         {
+            qtd_coincidentes++;
+         }
+         
+      }
+
+      if (qtd_coincidentes == 0)
+      {
+         append(qtdUnicos, set3, set1[i]);
+      }
+   }
+
+   // Percorre o segundo array e verifica se tem 1 elemento igual no primeiro array
+   for (int i = 0; i < set_size_2; i++)
+   {
+      int qtd_coincidentes = 0;
+      for (int j = 0; j < set_size_1; j++)
+      {
+         if (set2[i] == set1[j])
+         {
+            qtd_coincidentes++;
+         }
+         
+      }
+
+      if (qtd_coincidentes == 0)
+      {
+         append(qtdUnicos, set3, set2[i]);
+      }
+   }
+
+   bubble(qtdUnicos, set3);
+
+   for (int i = 0; i < qtdUnicos; i++)
+   {
+      printf("%d ", set3[i]);
+   }
 }
 
 void bubble(int size, int *numbers){
@@ -75,6 +129,17 @@ void bubble(int size, int *numbers){
             numbers[i] = numbers[i - 1];
             numbers[i - 1] = aux;
          }
+      }
+   }
+}
+
+void append(int size, int *arr, int n){
+   for (int i = 0; i < size; i++)
+   {
+      if (arr[i] == 0)
+      {
+         arr[i] = n;         
+         break;
       }
    }
 }
