fa9e8460a725c0c96ab1d1319407ddf436f1168a
Lucas Vinícius <lucas4cristal@gmail.com>
2022-09-30 13:28:44 -0300

    Gambiarra da pirâmide feita

--- Unidade 1/Week 6/LoP/Prova/piramide.c
+++ Unidade 1/Week 6/LoP/Prova/piramide.c
@@ -12,43 +12,47 @@ int main(){
 }
 
 void mostrar_piramide(int largura){
-   int passo = largura - 2;
-
-   // if (largura % 2 == 0)
-   // {
-   //    /* code */
-   // }
-   // else{
-
-   // }
+   int passo = largura;
    
    // Laço que percorre as linhas do array
-   for (int i = 0; i < largura; i++)
+   for (int i = 1; i <= largura; i++)
    {
       // Laço que percorre as colunas do array
-      for (int j = 0; j < largura; j++)
+      for (int j = 1; j <= largura; j++)
       {
-         // Se o i for a primeira ou a última linha
-         if (i == 0 && i == largura - 1)
+
+         if (i == 1 || i == largura || j == 1 || j == largura)
          {
-            // Printa a primeira linha da piramide
-            for (int k = 0; k < largura; k++)
-            {
-               if (k != largura - 1)
-               {
-                  printf("%d ", i + 1);
-               }
-               else
-               {
-                  printf("%d\n", i + 1);
-               }
-            }
-            
-         }
-         else
+            printf("1 ");
+         }else if (i == 2 || i == largura - 1 || j == 2 || j == largura - 1)
          {
-            
+            printf("2 ");
          }
+         else if (i == 3 || i == largura - 2 || j == 3 || j == largura - 2)
+         {
+            printf("3 ");
+         }
+         else if (i == 4 || i == largura - 3 || j == 4 || j == largura - 3)
+         {
+            printf("4 ");
+         }
+         else if (i == 5 || i == largura - 4 || j == 5 || j == largura - 4)
+         {
+            printf("5 ");
+         }
+          else if(i == j && j < largura){
+            printf("%d ", j); 
+         }
+         else{
+
+            printf("%d ", i);
+         }
+
+
+         
+
       }
+
+      printf("\n");
    }
 }
