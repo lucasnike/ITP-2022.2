aae6ab3c29ee7197b35be2da74cff73bb6be3c8e
Lucas Vinícius <lucas4cristal@gmail.com>
2022-10-11 13:30:42 -0300

    Uma nova esperança

--- Unidade 2/Week 7/LoP/Prova/criptografia.c
+++ Unidade 2/Week 7/LoP/Prova/criptografia.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
-char* criptografar(char *mensagem);
+int* shape(char *chave, int string_size);
+void read_line( char linha[], int tam );
 
-int main(){
-   const char DIGITS[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
-   
+int main(){   
    const char S[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 
    'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O',
    'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '.', ',', '?', ' ' };
@@ -13,30 +14,26 @@ int main(){
    char msg[200] = {0};
 
    scanf("%s", &k);
-   scanf("%s", &msg);
+   read_line(msg, 200);
 
+   int msg_size = (int)strlen(msg);
 
+   // Verifica a chave
    for (int i = 0; i < sizeof(k); i++)
    {
-      int iguais = 0;
-      for (int j = 0; j < sizeof(DIGITS); j++)
-      {
-         if(k[i] == DIGITS[j]){
-            iguais++;
-         }
-      }
-      
-      if (iguais == 0)
+      if (k[i] < '0' || k[i] > '9')
       {
          printf("Chave invalida!");
          return 0;
       }
+      
    }
    
-   for (int i = 0; i < sizeof(msg); i++)
+   // Verficida se a mensagem é válida
+   for (int i = 0; i < 200; i++)
    {
       int iguais = 0;
-      for (int j = 0; j < sizeof(S); j++)
+      for (int j = 0; j < 40; j++)
       {
          if(msg[i] == S[j]){
             iguais++;
@@ -50,9 +47,66 @@ int main(){
       }
    }
 
+   int *format = shape(k, msg_size);
+
+   for (int i = 0; i < msg_size; i++)
+   {
+     for (int j = 0; j < 40; j++)
+     {
+         if (msg[i] == S[j])
+         {
+            int position = j + format[i];
+
+            if (position > 39)
+            {
+               position - 39;
+            }
+
+            msg[i] = S[position];
+            printf("%c\n", msg[i]);
+            break;
+         }
+     }
+   }
+
+   for (int i = 0; i < msg_size; i++)
+   {
+      printf("%c", msg[i]);
+   }
+   
    return 0;
 }
 
-char* criptografar(char *mensagem){
+int* shape(char *chave, int string_size){
+   int *format;
+   format = calloc(string_size, sizeof(int));
+   int contador = 0;
 
+   for (int i = 0; i < string_size; i++)
+   {
+      format[i] = chave[contador] - 48;
+
+      if (contador == 3)
+      {
+         contador = -1;
+      }
+
+      contador++;
+   }
+
+   return format;
+}
+
+void read_line( char linha[], int tam ){
+
+   fgets( linha, tam, stdin );
+
+   while( linha[0] == '\n' )
+   {
+      fgets( linha, tam, stdin);    
+   }
+   if( linha[ strlen(linha) - 1 ] == '\n' ){
+
+      linha[ strlen(linha) - 1 ] = '\0';      
+   }  
 }
